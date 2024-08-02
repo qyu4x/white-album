7336cb0b45821fadf1889709dc00377b4b155d56
Ariq Khoiri <qq.khoiri@gmail.com>
2024-08-02 19:21:48 +0700

    feat: find in string functionality to search chunk of kata in string data

--- src/data_structures/array_and_string/StringImpl.java
+++ src/data_structures/array_and_string/StringImpl.java
@@ -5,11 +5,48 @@ import java.util.Arrays;
 
 class DSAString {
 
+    private String data;
+
+    public DSAString(String data) {
+        this.data = data;
+    }
+
+    public DSAString() {
+    }
+
     // string is bytes of array
     public byte[] behindString(String kata) {
         byte[] bytes = kata.getBytes(StandardCharsets.UTF_8);
         System.out.println(Arrays.toString(bytes));
         return bytes;
+    }
+
+    public void findInString(String kata) {
+        int lengthOfKata = kata.length();
+        byte[] bytesOfKata = kata.getBytes();
+        byte[] bytesOfData = this.data.getBytes();
+
+        int indexKata = 0;
+        int foundCounterKata = 0;
+        int startIndex = 0;
+        for (int i = 0; i < this.data.length(); i++) {
+            if (bytesOfKata[indexKata] == bytesOfData[i]) {
+                if (foundCounterKata == 0) startIndex = i;
+                foundCounterKata++;
+                indexKata++;
+
+                if (foundCounterKata == lengthOfKata) {
+                    System.out.println(String.format("Found at index: %s", startIndex));
+                    foundCounterKata = 0;
+                    indexKata = 0;
+                    startIndex = 0;
+                }
+            } else {
+                foundCounterKata = 0;
+                indexKata = 0;
+                startIndex = 0;
+            }
+        }
     }
 
 
@@ -19,8 +56,9 @@ class DSAString {
 public class StringImpl {
     public static void main(String[] args) {
 
-        DSAString stringChan = new DSAString();
-        System.out.println(new String(stringChan.behindString("Sukiyo")));
-
+//        DSAString stringChan = new DSAString();
+//        System.out.println(new String(stringChan.behindString("Sukiyo")));
+        DSAString stringChan = new DSAString("geeksforgeeks a computer sciencegeeks");
+        stringChan.findInString("geeks");
     }
 }
